dd5f192b7294fdef47bc3dd20c6cc806ae1ed77c
Vektor Zero <vektorzero0@gmail.com>
2026-09-01 01:28:23 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,12 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+});
+
+pool.on('error', (err) => {
+    console.error('❌ Database Error:', err.message);
 });
 
 module.exports = pool;
