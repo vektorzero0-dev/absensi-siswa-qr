2e9b2beb1b1ab0024ec96ae841c317acb3ffd46a
Vektor Zero <vektorzero0@gmail.com>
2026-08-31 13:11:30 +0700

    Initialize database connection with pg and dotenv

---
+++ db.js
@@ -0,0 +1,9 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+});
+
+module.exports = pool;
