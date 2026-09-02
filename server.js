a611bde55d82d9fd9c01496f5367cd07e6283fcf
Vektor Zero <vektorzero0@gmail.com>
2026-09-02 21:44:25 +0700

    Update print statement from 'Hello' to 'Goodbye'

--- server.js
+++ server.js
@@ -896,5 +896,5 @@ app.get('/api/absensi/export', async (req, res) => {
         res.status(500).send("Gagal mengekspor data: " + err.message);
     }
 });
-
+app.get('/ping', (req, res) => res.send('OK'));
 app.listen(PORT, () => console.log(`🚀 Server Presensi Aktif di Port ${PORT}`));
